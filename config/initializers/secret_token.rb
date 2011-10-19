db171f33e47df5356bfbf6d493bda3ce629d2d26
Casey Leask <caseyleask@gmail.com>
2011-10-19 16:51:28 +1100

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RozellaFlats::Application.config.secret_token = '9513f35f77ca2f283ee044b0d3d6dcc9886c29ac1a52bb9b21ce0a2ae10111ab00aed972309bfc12dea31e5a89bfddda13742859994ebaffce28ee0b15cb4557'
